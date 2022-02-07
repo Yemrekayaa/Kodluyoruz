6a7655205808b536546454eb7abd0d139657d08f
Yunus Emre Kaya <yemrekayatr@gmail.com>
2022-02-07 17:54:12 +0300

    JAVASCRIPT-3

---
+++ JS/js-3/js/app.js
@@ -0,0 +1,143 @@
+const menu = [
+  {
+    id: 1,
+    title: "Tteokbokki",
+    category: "Korea",
+    price: 10.99,
+    img:
+      "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+    desc: `Spicy rice cakes, serving with fish cake.`,
+  },
+  {
+    id: 2,
+    title: "Chicken Ramen",
+    category: "Japan",
+    price: 7.99,
+    img:
+      "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+    desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+  },
+  {
+    id: 3,
+    title: "Bibimbap",
+    category: "Korea",
+    price: 8.99,
+    img:
+      "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+    desc: `Boiling vegetables, serving with special hot sauce`,
+  },
+  {
+    id: 4,
+    title: "Dan Dan Mian",
+    category: "China",
+    price: 5.99,
+    img:
+      "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+    desc: `Dan dan noodle, serving with green onion `,
+  },
+  {
+    id: 5,
+    title: "Yangzhou Fried Rice",
+    category: "China",
+    price: 12.99,
+    img:
+      "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+    desc: `Yangzhou style fried rice, serving with bean and pickles `,
+  },
+  {
+    id: 6,
+    title: "Onigiri",
+    category: "Japan",
+    price: 9.99,
+    img:
+      "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+    desc: `Rice Sandwich, serving with soy sauce`,
+  },
+  {
+    id: 7,
+    title: "Jajangmyeon",
+    category: "Korea",
+    price: 15.99,
+    img:
+      "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+    desc: `Black bean sauce noodle, serving with green onion `,
+  },
+  {
+    id: 8,
+    title: "Ma Yi Shang Shu",
+    category: "China",
+    price: 12.99,
+    img:
+      "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+    desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+  },
+  {
+    id: 9,
+    title: "Doroyaki",
+    category: "Japan",
+    price: 3.99,
+    img:
+      "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+    desc: `Red bean paste dessert, serving with honey.`,
+  },
+];
+
+
+const section_center = document.querySelector(".section-center");
+
+const categories = menu.reduce((ctgrs, item) => {
+  if (!ctgrs.includes(item.category)) {
+    ctgrs.push(item.category);
+  }
+  return ctgrs;
+}, ["All"]);
+
+
+
+const btn_container = document.querySelector(".btn-container");
+categories.forEach(item => {
+  let c_button = document.createElement("button");
+  c_button.classList.add("btn", "btn-outline-dark", "btn-item");
+  c_button.id = item;
+  c_button.innerHTML = item;
+  c_button.onclick = () =>{ 
+    let filterCategories = item != "All" ? menu.filter((items) => {
+    if(items.category == item){
+       return items;
+     }
+    }) : menu;
+    menuList(filterCategories,item);
+    
+  };
+  btn_container.append(c_button);
+});
+let buttons = document.querySelectorAll(".btn-item");
+menuList(menu,"All");
+
+function menuList(menu,menuName) {
+  section_center.innerHTML = "";
+  document.querySelector(`#${menuName}`).classList.replace("btn-outline-dark","btn-dark");
+
+  buttons.forEach(item =>{ if(item.id != menuName)item.classList.replace("btn-dark","btn-outline-dark")});
+  menu.forEach(item => {
+    let menu_item = document.createElement("div");
+    menu_item.classList.add("menu-items","col-lg-6","col-sm-12");
+    menu_item.innerHTML = `
+      <img
+        src=${item.img}
+        alt=${item.title}
+        class="photo"
+      />
+      <div class="menu-info">
+        <div class="menu-title">
+          <h4>${item.title}</h4>
+          <h4 class="price">${item.price}</h4>
+        </div>
+        <div class="menu-text">
+          ${item.desc}
+        </div>
+      </div>`;
+    section_center.append(menu_item);
+  });
+};
+
