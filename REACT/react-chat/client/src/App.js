793b2455a44d575d321da5a05dd42a0b2b6d988b
Yunus Emre Kaya <yemrekayatr@gmail.com>
2022-02-27 07:05:20 +0300

    REACT-CHAT

---
+++ REACT/react-chat/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Container from './components/Container'
+import { ChatProvider } from './context/ChatContext';
+
+
+function App() {
+  return (
+    <ChatProvider>
+      <Container/>
+    </ChatProvider>
+  );
+}
+
+export default App;
---
+++ REACT/react-chat/client/src/components/Container.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import ChatList from './ChatList'
+import ChatForm from './ChatForm'
+import { init, subscribeChat, subscribeInitialMessages } from '../SocketApi'
+import {useChat} from '../context/ChatContext'
+
+function Container() {
+    const {setMessages} = useChat();
+
+    useEffect(()=>{
+        init();
+
+        subscribeInitialMessages((messages)=> setMessages(messages) )
+
+        subscribeChat((message) => {
+            setMessages((prevState)=> [...prevState, {message}])
+        });
+        
+    },[])
+
+
+  return (
+    <div className='App'>
+        <ChatList/>
+        <ChatForm/>
+    </div>
+  )
+}
+
+
+export default Container
