e94ae24f9be5a575b92a780bd91c422730910995
Yunus Emre Kaya <yemrekayatr@gmail.com>
2022-04-04 00:17:05 +0300

    C# While-Foreach

---
+++ C#/While-Foreach/Program.cs
@@ -0,0 +1,28 @@
+﻿//While
+
+// 1 den başlayarak console'dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+Console.Write("Lütfen bir sayı giriniz : ");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+while (sayac <= sayi)
+{
+    toplam += sayac;
+    sayac++;
+}
+Console.WriteLine(toplam / sayi);
+
+// a dan z ye kadar tüm harfleri console a yazdır.
+char character = 'a';
+while (character < 'z')
+{
+    Console.Write(character);
+    character++;
+}
+
+//Foreach
+string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
