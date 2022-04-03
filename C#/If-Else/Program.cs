b37f1ee2fab97b28381d79c5a3dfd5da5d1c774a
Yunus Emre Kaya <yemrekayatr@gmail.com>
2022-04-03 23:58:45 +0300

    C# if-else

---
+++ C#/If-Else/Program.cs
@@ -0,0 +1,18 @@
+﻿int time = DateTime.Now.Hour;
+
+if (time >= 6 && time < 11)
+{
+    Console.WriteLine("Günaydın !");
+}
+else if (time <= 18)
+{
+    Console.WriteLine("İyi Günler !");
+}
+else
+{
+    Console.WriteLine("İyi Geceler !");
+}
+
+string sonuc = time <= 18 ? "İyi Günler !" : "İyi Geceler !";
+sonuc = time >= 6 && time < 11 ? "Günaydın !" : time <= 18 ? "İyi Günler !" : "İyi Geceler !";
+Console.WriteLine(sonuc);
