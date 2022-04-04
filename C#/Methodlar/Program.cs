234f01def05808858a2e50741211be43483fa300
Yunus Emre Kaya <yemrekayatr@gmail.com>
2022-04-04 17:21:27 +0300

    C# Methodlar

---
+++ C#/Methodlar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erişim_belirteci    geri_dönüş_tipi    metot_adı(parametre listesi / argüman)
+            //{
+                //komutlar;
+                //return;
+            //}
+
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a + b);
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a + b));
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+}
