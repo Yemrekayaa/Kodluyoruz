bacfecd4a8e191e0198531eb52e3605a2e1cd1f3
Yunus Emre Kaya <yemrekayatr@gmail.com>
2022-04-13 02:48:22 +0300

    C# Enum

---
+++ C#/Enum/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"{Gunler.Pazar}");
+        Console.WriteLine($"{(int)Gunler.Cumartesi}");
+
+        int sicaklik = 32;
+
+        if(sicaklik <= (int)HavaDurumu.Normal)
+            Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınımasını bekleyelim.");
+        else if(sicaklik >= (int)HavaDurumu.Sicak)
+            Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+        else if(sicaklik >= (int)HavaDurumu.Normal)
+            Console.WriteLine("Hadi dışarıya çıkalım.");
+            
+    }
+}
+
+enum Gunler
+{
+    Pazartesi = 1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 5,
+    Normal = 20,
+    Sicak = 25,
+    CokSicak = 30
+}
