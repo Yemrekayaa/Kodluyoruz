aa7e945755703489fced36f1b64adbc9df8c40bf
Yunus Emre Kaya <yemrekayatr@gmail.com>
2022-04-13 02:29:37 +0300

    C# Struct

---
+++ C#/Struct/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Dikdortgen dikdortgen = new Dikdortgen();
+        Console.WriteLine($"Class Alan Hesabı: {dikdortgen.AlanHesapla()}");
+
+        Dikdortgen_Struct dikdortgen_Struct;
+        dikdortgen_Struct.kisaKenar = 3;
+        dikdortgen_Struct.uzunKenar = 4;
+        Console.WriteLine($"Struct Alan Hesabı: {dikdortgen_Struct.AlanHesapla()}");
+    }
+
+}
+
+class Dikdortgen
+{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public Dikdortgen()
+    {
+        kisaKenar = 3;
+        uzunKenar = 4;
+    }
+
+    public long AlanHesapla()
+    {
+        return kisaKenar * uzunKenar;
+    }
+}
+
+struct Dikdortgen_Struct
+{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public Dikdortgen_Struct(int KisaKenar, int UzunKenar)
+    {
+        kisaKenar = KisaKenar;
+        uzunKenar = UzunKenar;
+    }
+
+    public long AlanHesapla()
+    {
+        return kisaKenar * uzunKenar;
+    }
+}
